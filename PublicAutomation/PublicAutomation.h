b720105753db31c25655aa0d10496a962ee3cc43
Pete Hodgson <git@thepete.net>
2012-08-12 12:17:53 -0700

    initial project

---
+++ PublicAutomation/PublicAutomation.h
@@ -0,0 +1,13 @@
+//
+//  PublicAutomation.h
+//  PublicAutomation
+//
+//  Created by Pete Hodgson on 8/12/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PublicAutomation : NSObject
+
+@end
---
+++ PublicAutomation/PublicAutomation.m
@@ -0,0 +1,13 @@
+//
+//  PublicAutomation.m
+//  PublicAutomation
+//
+//  Created by Pete Hodgson on 8/12/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import "PublicAutomation.h"
+
+@implementation PublicAutomation
+
+@end
